a2c9c8c6e2205a5398c9204169f57227adc10685
Afterlife24 <afterlife668@gmail.com>
2024-12-23 22:27:28 +0530

    serverless_5

--- script.js
+++ script.js
@@ -33,7 +33,7 @@
 
 /* ----- TYPING EFFECT ----- */
    var typingEffect = new Typed(".typedText",{
-      strings : ["Designer","Youtuber","Developer"],
+      strings : ["Web Developer","Marketing","App Developer"],
       loop : true,
       typeSpeed : 100, 
       backSpeed : 80,
